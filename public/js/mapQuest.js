1fb604d924b8d242e22a575447f286e59ff6f7e4
Samuel Blake <samuelblake97@gmail.com>
2019-02-28 13:02:53 -0800

    Finished the Lab
    Used MapQuest API, Camera, and used a post request to implement a RSVP form.

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'iXfevYVibfhbAWnjRg1DoHV3I97lgvMy';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	//Add a marker to the map
+	L.marker([32.878974, -117.236]).addTo(map);
 }
